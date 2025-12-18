802301554a466f520f9a7043be5f7c99e9f8e92f
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2025-12-18 18:05:46 +0100

    test fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,30 @@
 import pytest
 from PyQt5.QtGui import QGuiApplication
 import sys
+import cv2
+from unittest.mock import patch
 from main import create_app
+from main import Backend
+import cv.roi_processor
+
+from unittest.mock import patch
+
+class DummyRoiProcessor:
+    def _mask_exporter(self, img):
+        # returns dummy mask coords
+        return [[0,0],[1,1]]
+    
+    def _mask_painter(self, image, mask_coords):
+        # Returns dummy image
+        img = cv2.imread(image)
+        return img 
+
+@pytest.fixture
+def mock_roi_processor(monkeypatch):
+    # Replace RoiProcessor with it's dummy testing version
+    monkeypatch.setattr(cv.roi_processor, "cv.RoiProcessor" , DummyRoiProcessor)
+
+
 
 def test_cv_worker(qtbot):
     # Create App
@@ -12,9 +35,9 @@ def test_cv_worker(qtbot):
      # Run the ROI pipeline
     backend.run_cv_roi_pipe()
 
-    # Wait for the signal from the backend
+    # As CvWorker starts new thread - waiting is required for the signal to comeback
+    # waiting for backend.roiImageUpdated
     with qtbot.waitSignal(backend.roiImageUpdated, timeout=5000) as blocker:
-        pass  # the signal will be captured here
+        pass 
 
-    # Assert that the backend stored the image
     assert backend.get_roi_img() is not None, "Mask image was not returned from get_roi_img"
