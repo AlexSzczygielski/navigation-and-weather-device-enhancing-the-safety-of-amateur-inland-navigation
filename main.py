a7499fa79b27365f3c6733f4d644da054a1ff066
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2025-12-18 20:23:14 +0100

    refactored main.py code structure (#35)

--- main.py
+++ main.py
@@ -9,25 +9,25 @@ from factories.create_backend import create_backend
 
 def create_app():
     # Composition function - it wires together all components required to start an app
+    # Create Qt app and QML engine (view)
     app = QGuiApplication(sys.argv)
     view = QQmlApplicationEngine()
     view.addImportPath(sys.path[0])
     
-    #backend = create_backend()
+    # Create backend class, expose it to QML engine. 
+    # Load qrc resources
     backend = create_backend()
     view.rootContext().setContextProperty("backend",backend)
-
-    #view.load("App/views/home.qml")
     view.load(QUrl("qrc:main.qml"))
 
     return app, view, backend
 
 
 if __name__ == "__main__":
-    # Create app and run it
+    # Entry point - create app and run it
     app, view, backend = create_app()
     ex = app.exec()
 
-    # After Qt app finish
+    # After Qt app finishes
     del view
     sys.exit(ex)
