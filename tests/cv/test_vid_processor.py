35093a1710dec670ed8e09a26fc16e030470788e
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2026-01-21 02:23:49 +0100

    updated tests

--- tests/cv/test_vid_processor.py
+++ tests/cv/test_vid_processor.py
@@ -5,14 +5,17 @@
 import pytest
 from cv.video_processor import VideoProcessor
 import config
+from multiprocessing import Event
 
 dummy_mask = [[0,0],[1,1]]
 
 def test_run_video_inference():
     processor = VideoProcessor(config.MODEL_WEIGHTS["yolo11"], config.DEMO_ASSETS["video2"], dummy_mask)
 
+    stop_event = Event()
+
     try:
-        generator = processor.run_video_inference()
+        generator = processor.run_video_inference(stop_event)
         frame = next(generator)
         assert frame is not None
     except Exception as e:
